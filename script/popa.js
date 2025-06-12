e1416bd002fa3a32d2164b5e4adf01cf17f5fc3b
Patfither <sultanrymbaj@gmail.com>
2025-06-12 20:05:10 +0500

    Add files via upload

---
+++ script/popa.js
@@ -0,0 +1,15 @@
+function checkEmail() {
+    let email = document.querySelector('#emailField').value;
+
+    if (!email.includes('@')) {
+        alert('Нет символа @');
+    }
+
+    else if (!email.includes('.')) {
+        alert('Нет символа .');
+    }
+
+    else {
+        alert('Все отлично');
+    }
+}
